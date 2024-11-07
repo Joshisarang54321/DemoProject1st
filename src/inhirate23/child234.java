b01fd097619108c162b2e71190f87eff2156dc9b
joshisarang54321 <joshisarang54321@gmail.com>
2024-11-08 00:44:27 +0530

    type commit message

---
+++ src/inhirate23/child234.java
@@ -0,0 +1,14 @@
+package inhirate23;
+
+
+public class child234 extends inhiratenew234{
+
+	
+		public static void main(String[] args) {
+			child234 obj=new child234 ();
+			obj.name();
+			
+
+	}
+
+}
---
+++ src/inhirate23/inhiratenew234.java
@@ -0,0 +1,11 @@
+package inhirate23;
+
+public class inhiratenew234 {
+	public void name()
+    {
+		
+		System.out.println("Sarang From this side");
+
+	}
+
+}
