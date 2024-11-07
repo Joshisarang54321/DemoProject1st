2ed2b270a1b8c090094f7baacccf3ece4b9bb293
joshisarang54321 <joshisarang54321@gmail.com>
2024-11-08 00:36:05 +0530

    this program gets single inhirate program

---
+++ src/Inhirit/Inhiritance1stgit.java
@@ -0,0 +1,11 @@
+package Inhirit;
+
+public class Inhiritance1stgit {
+	public void demo ()
+    {
+		
+		System.out.println("this method belongs gto parents class");
+
+	}
+
+}
---
+++ src/Inhirit/childcl.java
@@ -0,0 +1,13 @@
+package Inhirit;
+
+public class childcl extends Inhiritance1stgit  {
+
+	public static void main(String[] args) {
+		childcl obj=new childcl ();
+		obj.demo();
+		
+		
+
+	}
+
+}
